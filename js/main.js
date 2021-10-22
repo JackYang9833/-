07870070a58ac8960ef20a65235406be38c3b4f4
JackYang9833 <79178595+JackYang9833@users.noreply.github.com>
2021-10-22 08:04:11 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(getData=function() {
+  $.ajax({
+    url: "https://v1.hitokoto.cn",
+    dataType: "json",
+    data:{c:'i'},
+    async: false,
+    success: function(data) {
+      // $("p").append(data + "<br />");
+      $('#word').text(data.hitokoto);
+      $('#author').text(data.from_who+"《" +data.from+"》");
+      console.log(data);
+    }
+  });
+});
+
+setInterval(getData,5000)
