ae21bc4c9eaa9913e3c5d271f675767cb52fd4b9
JackYang9833 <79178595+JackYang9833@users.noreply.github.com>
2021-10-24 12:39:40 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,14 +18,9 @@ function getData(){
   })
 }
 setInterval(getData,5000)
-  var i=0;
 let load="<div id=\"preloader\">Page is loading,please wait a while...</div>"+i;
 $('body').append(load);
 document.onreadystatechange=function(){
-  while(document.readyState=="loading"){
-    i++;
-    $('#preloader').text(i)
-  }
   if(document.readyState=="complete"){
     $('#preloader').hide();
   }
