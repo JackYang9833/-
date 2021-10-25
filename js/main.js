05c5aa31344a6bfb45934d16889135c23a4ef563
JackYang9833 <79178595+JackYang9833@users.noreply.github.com>
2021-10-25 08:45:30 +0800

    update main.js

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ var str=" <div id=\"circularG\">"+
         "<div id=\"circularG_7\" class=\"circularG\"></div>"+
         "<div id=\"circularG_8\" class=\"circularG\"></div>"+
      "</div>";
-$('.circular-wrapper').append();
+$('.circular-wrapper').append(str);
 document.onreadystatechange=function(){
   if(document.readyState=="complete"){
     $('#circularG').fadeOut();
